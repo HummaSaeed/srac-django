f7a2fe04c51cfa09cf7198984d9de466fd77f4db
HummaSaeed <84771295+HummaSaeed@users.noreply.github.com>
2023-05-28 16:58:49 +0500

    staging

--- crud/serializers.py
+++ crud/serializers.py
@@ -5,5 +5,5 @@ from .models import Item
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = ('username','name','brand', 'modal', 'category', 'year','distance','img', 'transmissionType', 'bodyType', 'price', 'condition', 'paymentoption','created','updated')
+        fields = ('username','name','brand', 'modal', 'category', 'year','mindistance','maxdistance','installment','distance','img', 'transmissionType', 'bodyType', 'price', 'condition', 'paymentoption','created','updated')
 
