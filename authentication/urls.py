afa58fa50461881f89a113b99842701b56383327
HummaSaeed <84771295+HummaSaeed@users.noreply.github.com>
2023-05-28 15:41:05 +0500

    updates

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin', admin.site.urls),
     path('',include('accounts.urls')),
-    path('',include('crud.urls'))
-]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('',include('crud.urls')),
+    path('',include(settings.MEDIA_URL))
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
