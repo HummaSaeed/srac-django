f4b78d7364f43854134b6d992d0570cb14f5316d
HummaSaeed <84771295+HummaSaeed@users.noreply.github.com>
2023-05-28 16:03:22 +0500

    updates

--- authentication/urls.py
+++ authentication/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('admin', admin.site.urls),
     path('',include('accounts.urls')),
     path('',include('crud.urls')),
-    path('',include(settings.MEDIA_URL))
+    
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
