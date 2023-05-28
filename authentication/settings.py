5190abaf3e2fa789d0ad03433bdbf3ba87b4b195
HummaSaeed <84771295+HummaSaeed@users.noreply.github.com>
2023-05-28 04:33:58 -0700

    Update settings.py

--- authentication/settings.py
+++ authentication/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^l8^#gop2$+a0)d0w4*(lc_lr90vrfegnj7bpc3b$8vhsr-jvs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['admin.srac.me']
 
