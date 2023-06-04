21e25f75ba277482bce9c2764df14647feff3031
HummaSaeed <84771295+HummaSaeed@users.noreply.github.com>
2023-06-05 00:09:28 +0500

    settings

--- authentication/settings.py
+++ authentication/settings.py
@@ -79,13 +79,13 @@ WSGI_APPLICATION = 'authentication.wsgi.application'
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = [
-    'http://admin.srac.me/api/login',
+    'http://admin.srac.me/admin',
     'http://localhost:3000',
     'http://example.com',
     'http://shop.srac.me',
 ]
 CORS_ALLOWED_ORIGINS = [
-    'http://admin.srac.me',
+    'http://admin.srac.me/admin',
     'http://localhost:3000/',
     'http://shop.srac.me',
 ]
