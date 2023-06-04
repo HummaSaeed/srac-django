7bd35c8a9c786185ff804dfb4ba340cc8e5f82f3
HummaSaeed <84771295+HummaSaeed@users.noreply.github.com>
2023-06-04 23:37:47 +0500

    commit

--- authentication/settings.py
+++ authentication/settings.py
@@ -86,6 +86,8 @@ CORS_ORIGIN_WHITELIST = [
 ]
 CORS_ALLOWED_ORIGINS = [
     'http://admin.srac.me',
+    'http://localhost:3000',
+    'http://shop.srac.me',
 ]
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
